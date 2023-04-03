db0c8c0b3310f66ddc817637e326c96affcc5904
Krishna <kirankrsna7@outlook.com>
2023-04-03 22:16:49 +0530

    final commit

---
+++ LittleLemon/LittleLemonDRF/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Category,MenuItem,Cart,Order,OrderItem
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+        list_display = ['slug', 'title' ]
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+        list_display = ['title', 'price', 'featured', 'category' ]
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+        list_display = ['user', 'menuitem', 'quantity', 'unit_price', 'price' ]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+        list_display = ['user', 'delivery_crew', 'status', 'total', 'date' ]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+        list_display = ['order', 'menuitem', 'quantity', 'price' ]
